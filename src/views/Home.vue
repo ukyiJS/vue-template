9c292de90b29bb5f02562fb07fe6e85cf16c0c13
UkYi <ukyi.js@gmail.com>
2020-02-29 19:56:00 +0900

    add home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <hellow-world />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HellowWorld from '~/components/HelloWorld';
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld,
-  },
+    HellowWorld
+  }
 };
 </script>
+
+<style scoped></style>
